01d77087de2b33f1be1ac7f328e831570480a772
Simon Morris <simonjmorris213@gmail.com>
2022-05-18 05:38:49 -0500

    commit

---
+++ src/main/java/com/example/application/views/list/ContactForm.java
@@ -0,0 +1,10 @@
+package com.example.application.views.list;
+
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.textfield.TextField;
+
+public class ContactForm extends FormLayout {
+    
+    TextField firstName = new TextField("First name");
+
+}
